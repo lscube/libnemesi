c111ffe8079058abfe924235602c2b230cadda8f
shawill <shawill>
2003-01-15 11:24:27 +0000

    Aggiunta del modulo di ouput "diskwriter"

---
+++ output/diskwriter/close_file.c
@@ -0,0 +1,40 @@
+/* * 
+ *  ./output/diskwriter/close_file.c: $Revision: 1.1 $ -- $Date: 2003/01/15 11:24:26 $
+ *  
+ *  This file is part of NeMeSI
+ *
+ *  NeMeSI -- NEtwork MEdia Streamer I
+ *
+ *  Copyright (C) 2001 by
+ *  	
+ *  	Giampaolo "mancho" Mancini - manchoz@inwind.it
+ *	Francesco "shawill" Varano - shawill@infinto.it
+ *
+ *  NeMeSI is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NeMeSI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NeMeSI; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ * */
+
+#include <stdio.h>
+
+void close_file(void)
+{
+	int fd;
+	
+	if ( (fd=get_file_fd()) > 0 )
+		close(fd);
+
+	return;
+}
+
