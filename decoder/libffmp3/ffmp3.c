dd245ed573a39e9b5cf9de1d49cb7b272cbddabc
sbiro <sbiro>
2003-04-03 10:43:55 +0000

    Pulitura dei commenti e aggiunta della licenza GPL

--- decoder/libffmp3/ffmp3.c
+++ decoder/libffmp3/ffmp3.c
@@ -1,3 +1,31 @@
+/* * 
+ *  ./decoder/libffmpeg/ffmpeg.c: $Revision: 1.2 $ -- $Date: 2003/04/03 10:43:55 $
+ *  
+ *  This file is part of NeMeSI
+ *
+ *  NeMeSI -- NEtwork MEdia Streamer I
+ *
+ *  Copyright (C) 2001 by
+ *  	
+ *  	Giampaolo "mancho" Mancini - manchoz@inwind.it
+ *	Francesco "shawill" Varano - shawill@infinto.it
+ *
+ *  NeMeSI is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NeMeSI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NeMeSI; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ * */
+
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
@@ -10,14 +38,6 @@
 #endif
 
 #include <ffmpeg/avcodec.h>
-
-/*#include "mpg123.h"
-#include "mpglib.h"*/
-
-/*#include <nemesi/comm.h>*/
-
-/* char buf[16384]; */
-/* struct mpstr mp; */
 
 #define BUFFER 8192 
 
@@ -32,14 +52,10 @@ int get_plugin_pt(void)
 int decode(char *data, int len, uint8 *(*ab_get)(uint32))
 {
 	
-	/*static struct mpstr *mp = NULL;*/
 	static	AVCodec *codec;
     	static AVCodecContext *c= NULL;
-
-	// int size;
-	int out_size;//=AVCODEC_MAX_AUDIO_FRAME_SIZE;
+	int out_size;
 	static int16_t out[AVCODEC_MAX_AUDIO_FRAME_SIZE];
-	//static uint8_t *out;
 	int len_tmp=0;
 	uint8 *audio_data;
 
@@ -62,19 +78,8 @@ int decode(char *data, int len, uint8 *(*ab_get)(uint32))
 			exit(1);
 		}
     
-//		out = (uint8_t *)malloc(AVCODEC_MAX_AUDIO_FRAME_SIZE * sizeof(uint8_t));
 	}
 
-	/*if(!mp){
-		if((mp=(struct mpstr *)malloc(sizeof(struct mpstr))) == NULL){
-			fprintf(stderr,"Cannot allocate memory!\n");
-			return 1;
-		}
-		InitMP3(mp);
-	}*/
-
-	
-	
 	while ( len_tmp < (len - 4) ) {
 		len_tmp += avcodec_decode_audio(c, (int16_t *)out, (int *)&out_size, (uint8_t *)(data + 4 + len_tmp), (int)(len  - 4 - len_tmp));
 		if (out_size > 0){
@@ -82,14 +87,6 @@ int decode(char *data, int len, uint8 *(*ab_get)(uint32))
 			memcpy(audio_data, out, out_size);
 		}
 	}
-
-	/*ret = decodeMP3(mp, data+4, len-4, out, BUFFER, &size);*/
-	/*while(ret == MP3_OK) {
-	--canc-	write(audio_fd, out, size); 
-		audio_data=(*ab_get)((uint32)size);
-		memcpy(audio_data, out, size);
-		ret = decodeMP3(mp, NULL, 0, out, BUFFER, &size);
-	}*/
 
 	return 0;
 }
