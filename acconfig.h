9daadc39a8ef402c94efea014d43b3e0b3e45abb
mancho <mancho>
2003-07-24 14:02:26 +0000

    Obsoleto con le nuove versioni degli AutoTools

--- acconfig.h
+++
@@ -1,9 +0,0 @@
-@BOTTOM@
-/* Define the default directory which
- * NeMeSI will look for plugins. */
-#undef NEMESI_PLUGIN_DIR_DEFAULT
-
-/* Define the name for the environment
- * variable that holds an alternative
- * path to NeMeSI plugins directory. */
-#undef NEMESI_PLUGIN_DIR_ENV
