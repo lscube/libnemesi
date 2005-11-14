1278435ac7a3bd60bd31c9b50eb57ffd6b9d30c4
shawill <shawill>
2005-11-14 12:07:38 +0000

    Makefile.am changed for a better svnrev.h file handling.

--- svnrev.h
+++ svnrev.h
@@ -1,7 +1,7 @@
 /* This header is just for keeping track of current SVN revision.
  * If this file is present in the sources tree then this is a development SVN
  * version.
- * Mon Nov 14 10:52:41 CET 2005
+ * Mon Nov 14 13:09:14 CET 2005
  * */
 
 #ifndef __SVNREV_H
