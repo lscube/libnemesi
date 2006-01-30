b10886d156e8c32fd09dc2cf33df81a8a367b8aa
shawill <shawill>
2006-01-30 17:48:10 +0000

    rtp framers implementation started

---
+++ libnms/rtp/rtpframers/rtpframer.h
@@ -0,0 +1,44 @@
+/* * 
+ *  $Id$
+ *  
+ *  This file is part of NeMeSI
+ *
+ *  NeMeSI -- NEtwork MEdia Streamer I
+ *
+ *  Copyright (C) 2006 by
+ *  	
+ *  	Giampaolo "mancho" Mancini - giampaolo.mancini@polito.it
+ *	Francesco "shawill" Varano - francesco.varano@polito.it
+ *
+ *  NeMeSI is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NeMeSI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NeMeSI; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ * */
+
+#ifndef RTPPTFRAMER_H_
+#define RTPPTFRAMER_H_
+
+#include "rtpframers.h"
+
+static int fill_buffer(struct rtp_session *, struct rtp_ssrc *, char *, size_t, uint32 *);
+static int fill_buffer_nonblock(struct rtp_session *, struct rtp_ssrc *, char *, size_t, uint32 *);
+
+#define RTPFRMR(x) rtpfrmr rtp_framer_##x =\
+{\
+	&served, \
+	fill_buffer, \
+	fill_buffer_nonblock \
+}
+
+#endif /*RTPPTFRAMER_H_*/
---
+++ libnms/rtp/rtpframers/rtpframers.h
@@ -0,0 +1,52 @@
+/* * 
+ *  $Id$
+ *  
+ *  This file is part of NeMeSI
+ *
+ *  NeMeSI -- NEtwork MEdia Streamer I
+ *
+ *  Copyright (C) 2006 by
+ *  	
+ *  	Giampaolo "mancho" Mancini - giampaolo.mancini@polito.it
+ *	Francesco "shawill" Varano - francesco.varano@polito.it
+ *
+ *  NeMeSI is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NeMeSI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NeMeSI; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ * */
+
+#ifndef RTPPTFRAMERS_H_
+#define RTPPTFRAMERS_H_
+
+#include <nemesi/rtp.h>
+
+typedef struct {
+	uint16 static_pt;
+	char *mime[]; // NULL terminated list of served mime tipes in the form "type/subtype"
+} rtpfrmr_info;
+
+typedef struct {
+	rtpfrmr_info *served;
+	int (*fill_buffer)(struct rtp_session *, struct rtp_ssrc *, char *, size_t, uint32 *);
+	int (*fill_buffer_nonblock)(struct rtp_session *, struct rtp_ssrc *, char *, size_t, uint32 *);
+} rtpfrmr;
+
+#define RTP_FRM_ERROR		-1
+#define RTP_DST_TOO_SMALL	-2
+
+#define BLOCKING 0
+#define NON_BLOCKING 1
+extern int (*rtp_framers[128][2])(struct rtp_session *, struct rtp_ssrc *, char *, size_t, uint32 *);
+
+#endif /*RTPPTFRAMERS_H_*/
