f4e7e230b8e3d3cc1a3337f317464ba8aa6e65a8
mancho <mancho>
2006-06-08 10:12:19 +0000

    decoder/gsm_amr_float/Makefile.am: 	- Fix a compile bug on x86_64.

--- svnrev.h
+++ svnrev.h
@@ -1,7 +1,7 @@
 /* This header is just for keeping track of current SVN revision.
  * If this file is present in the sources tree then this is a development SVN
  * version.
- * Tue Jun  6 18:33:04 CEST 2006
+ * Thu Jun  8 11:37:14 CEST 2006
  * */
 
 #ifndef __SVNREV_H
