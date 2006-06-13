0fe12a51c946ae85acdf83960db20b82f2cf4ad4
lu_zero <lu_zero>
2006-06-13 14:53:03 +0000

    Xiph.org code in rtp_xiph, license set

--- libnms/rtp/rtpparsers/rtp_vorbis.c
+++ libnms/rtp/rtpparsers/rtp_vorbis.c
@@ -44,66 +44,6 @@ static rtpparser_info served = {
 
 RTPPRSR(vorbis);
 
-//bitreader from libogg, move somewhere later or replace with gpl code
-//Copyright for the bitreader code Xiph.org, the code is under a BSD-STYLE
-//license please check libogg sources for the full text.
-
-static const unsigned long mask[]=
-{0x00000000,0x00000001,0x00000003,0x00000007,0x0000000f,
- 0x0000001f,0x0000003f,0x0000007f,0x000000ff,0x000001ff,
- 0x000003ff,0x000007ff,0x00000fff,0x00001fff,0x00003fff,
- 0x00007fff,0x0000ffff,0x0001ffff,0x0003ffff,0x0007ffff,
- 0x000fffff,0x001fffff,0x003fffff,0x007fffff,0x00ffffff,
- 0x01ffffff,0x03ffffff,0x07ffffff,0x0fffffff,0x1fffffff,
- 0x3fffffff,0x7fffffff,0xffffffff };
-
-typedef bit_context {
-    int pos;
-    int left;
-    unsigned char *buf;
-}
-
-static long bit_read(bit_context *b,int bits){
-  long ret;
-  unsigned long m=mask[bits];
-
-  bits+=b->endbit;
-
-  if(b->endbyte+4>=b->storage){
-    /* not the main path */
-    ret=-1L;
-    if(b->endbyte*8+bits>b->storage*8)goto overflow;
-  }
-
-  ret=b->ptr[0]>>b->endbit;
-  if(bits>8){
-    ret|=b->ptr[1]<<(8-b->endbit);
-    if(bits>16){
-      ret|=b->ptr[2]<<(16-b->endbit);
-      if(bits>24){
-        ret|=b->ptr[3]<<(24-b->endbit);
-        if(bits>32 && b->endbit){
-          ret|=b->ptr[4]<<(32-b->endbit);
-        }
-      }
-    }
-  }
-  ret&=m;
-
- overflow:
-
-  b->ptr+=bits/8;
-  b->endbyte+=bits/8;
-  b->endbit=bits&7;
-  return(ret);
-}
-
-static void bit_readinit(bit_context *b, unsigned char *buf, int bytes){
-  memset(b,0,sizeof(*b));
-  b->buffer=b->ptr=buf;
-  b->left=bytes;
-}
-
 //helpers
 static int rtp_ilog(unsigned int v){
   int ret=0;
@@ -123,8 +63,7 @@ static int icount(unsigned int v){
   return(ret);
 }
 
-long 
-maptype_quantvals(int entries, int dim)
+static long maptype_quantvals(int entries, int dim)
 {
   long vals=floor(pow((float)entries,1.f/dim));
 
@@ -153,7 +92,7 @@ maptype_quantvals(int entries, int dim)
   }
 }
 
-int cfg_parse(rtp_vorbis *vorb, rtp_frame *fr) //FIXME checks!
+static int cfg_parse(rtp_vorbis *vorb, rtp_frame *fr) //FIXME checks missing!
 {
     bit_context_t opb;
     int num, i, j, k=-1, channels;
