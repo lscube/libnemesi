53eff9f687d57a7db85c4e3406d54c49cb7734a1
shawill <shawill>
2006-11-22 14:14:07 +0000

    Prepare for release 0.6.0

--- svnrev.h
+++ svnrev.h
@@ -1,7 +1,7 @@
 /* This header is just for keeping track of current SVN revision.
  * If this file is present in the sources tree then this is a development SVN
  * version.
- * Tue Oct 24 17:26:05 CEST 2006
+ * Wed Nov 22 12:20:12 CET 2006
  * */
 
 #ifndef __SVNREV_H
