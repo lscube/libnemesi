ea79786a5853a18775fbff21aa7047c6935301c1
lu_zero <lu_zero>
2007-02-25 22:22:12 +0000

    Cosmetics

--- libnms/example/dump_info.c
+++ libnms/example/dump_info.c
@@ -16,8 +16,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with libnms; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
- *  Description: sample program, takes a url and dumps the sdp and 
+ *
+ *  Description: sample program, takes a url and dumps the sdp and
  *  the information formatted per medium with proper descriptions.
  */
 
@@ -31,128 +31,129 @@
 #include <nemesi/rtp.h>
 #include <nemesi/sdp.h>
 
-
 int main(int argc, char **argv)
 {
 
-	int opt;
-	char *url, *out = "nemesi.dump";
-	FILE *outfile = NULL;
-	rtsp_ctrl *ctl;
-	rtsp_session *sess;
-	rtsp_medium *med;
-	nms_rtsp_hints rtsp_hints = { -1 };
-	sdp_attr *attr;
+    int opt;
+    char *url, *out = "nemesi.dump";
+    FILE *outfile = NULL;
+    rtsp_ctrl *ctl;
+    rtsp_session *sess;
+    rtsp_medium *med;
+    nms_rtsp_hints rtsp_hints = { -1 };
+    sdp_attr *attr;
 
-	if (argc < 2) {
-		fprintf(stderr, "\tPlease specify at least an url.\n");
-		fprintf(stderr,
-			"\tUsage: %s [-f outputfile ] [-p rtp_port] url\n",
-			argv[0]);
-		exit(1);
-	}
+    if (argc < 2) {
+        fprintf(stderr, "\tPlease specify at least an url.\n");
+        fprintf(stderr,
+            "\tUsage: %s [-f outputfile ] [-p rtp_port] [-t url\n",
+            argv[0]);
+        exit(1);
+    }
 
-	while ((opt = getopt(argc, argv, "df:p:v:")) != -1) {
-		switch (opt) {
+    while ((opt = getopt(argc, argv, "df:p:v:")) != -1) {
+        switch (opt) {
 
-			/*  Set output file  */
-		case 'f':
-			out = strdup(optarg);
-			break;
-			/*  Set rtp port  */
-		case 'p':
-			rtsp_hints.first_rtp_port = atoi(optarg);
-			break;
-			/*  Set verbosity  */
-		case 'v':
-			nms_verbosity_set(atoi(optarg));
-			break;
-			/* Unknown option  */
-		case '?':
-			fprintf(stderr, "\n  Unknown option `-%c'.\n", optopt);
-			fprintf(stderr,
-				"\tUsage: %s [-f outputfile ] [-d] url\n\n",
-				argv[0]);
+            /*  Set output file  */
+        case 'f':
+            out = strdup(optarg);
+            break;
+            /*  Set rtp port  */
+        case 'p':
+            rtsp_hints.first_rtp_port = atoi(optarg);
+            break;
+            /*  Set verbosity  */
+        case 'v':
+            nms_verbosity_set(atoi(optarg));
+            break;
+            /* Unknown option  */
+        case '?':
+            fprintf(stderr, "\n  Unknown option `-%c'.\n", optopt);
+            fprintf(stderr,
+                "\tUsage: %s [-f outputfile ] [-d] url\n\n",
+                argv[0]);
 
-			return 1;
-		}
-	}
+            return 1;
+        }
+    }
 
-	outfile = fopen(out, "rb");
-	if (outfile == NULL)
-		outfile = stderr;
+    outfile = fopen(out, "rb");
+    if (outfile == NULL)
+        outfile = stderr;
 
-	url = argv[argc - 1];
+    url = argv[argc - 1];
 
-	fprintf(stderr, "URL %s.\n", url);
+    fprintf(stderr, "URL %s.\n", url);
 
-	/* initialize the rtsp state machine, starts the rtsp and the rtp threads
-	 * the hints available are just one:
-	 *  - the first port to use (instead of picking one at random)
-	 */
-	if ((ctl = rtsp_init(&rtsp_hints)) == NULL) {
-		fprintf(stderr, "Cannot init rtsp.\n");
-		return 1;
-	}
+    /* initialize the rtsp state machine, starts the rtsp and the rtp threads
+     * the hints available are just one:
+     *  - the first port to use (instead of picking one at random)
+     */
+    if ((ctl = rtsp_init(&rtsp_hints)) == NULL) {
+        fprintf(stderr, "Cannot init rtsp.\n");
+        return 1;
+    }
 
-	if (rtsp_open(ctl, url)) {
-		fprintf(stderr, "rtsp_open failed.\n");
-		// die
-		return 1;
-	}
-	// you must call rtsp_wait after issuing any command
-	rtsp_wait(ctl);
+    if (rtsp_open(ctl, url)) {
+        fprintf(stderr, "rtsp_open failed.\n");
+        // die
+        return 1;
+    }
+    // you must call rtsp_wait after issuing any command
+    rtsp_wait(ctl);
 
-	// Get the session information
-	sess = ctl->rtsp_queue;
+    // Get the session information
+    sess = ctl->rtsp_queue;
 
-	if (!sess) {
-		fprintf(stderr, "No session available.\n");
-		return 1;
-	}
+    if (!sess) {
+        fprintf(stderr, "No session available.\n");
+        return 1;
+    }
 
-	while (sess) {		// foreach session...
-		fprintf(outfile, "\tSession %s\n", sess->pathname);
+    while (sess) {        // foreach session...
+        fprintf(outfile, "\tSession %s\n", sess->pathname);
 
-		med = sess->media_queue;
-		while (med) {	//... foreach medium
-			switch (med->medium_info->media_type) {
-				// Just care about audio and video
-			case 'A':
-			case 'V':
-				fprintf(outfile, "\tTransport %s\n",
-					med->medium_info->transport);
-				fprintf(outfile, "\tMedia format %s\n",
-					med->medium_info->fmts);
-				break;
-			default:
-				//do nothing
-				break;
-			}
+        med = sess->media_queue;
+        while (med) {    //... foreach medium
+            switch (med->medium_info->media_type) {
+                // Just care about audio and video
+            case 'A':
+            case 'V':
+                fprintf(outfile, "\tTransport %s\n",
+                        med->medium_info->transport);
+                fprintf(outfile, "\tMedia format %s\n",
+                        med->medium_info->fmts);
+                break;
+            default:
+                //do nothing
+                break;
+            }
 
-			// attributes are already parsed, get them from the list
-			for (attr = med->medium_info->attr_list; attr;
-			     attr = attr->next)
-				fprintf(outfile, "\t* %s\n", attr->a);
+            // attributes are already parsed, get them from the list
+            for (attr = med->medium_info->attr_list;
+                 attr;
+                 attr = attr->next) {
+                fprintf(outfile, "\t* %s\n", attr->a);
+            }
 
-			med = med->next;
-		}
-		sess = sess->next;
-	}
+            med = med->next;
+        }
+        sess = sess->next;
+    }
 
-	/*
-	 * Close the rtsp connection, we are polite
-	 */
+    /*
+     * Close the rtsp connection, we are polite
+     */
 
-	rtsp_close(ctl);
-	rtsp_wait(ctl);
+    rtsp_close(ctl);
+    rtsp_wait(ctl);
 
-	/*
-	 * Kill the threads, dealloc everything.
-	 */
+    /*
+     * Kill the threads, dealloc everything.
+     */
 
-	rtsp_uninit(ctl);
-	rtsp_wait(ctl);
+    rtsp_uninit(ctl);
+    rtsp_wait(ctl);
 
-	return 0;
+    return 0;
 }
--- libnms/example/dump_stream.c
+++ libnms/example/dump_stream.c
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with libnms; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
+ *
  *  Description: sample program, takes an url and dumps the demuxed streams to
  *  files.
  */
@@ -34,127 +34,124 @@
 
 int main(int argc, char **argv)
 {
+    int opt, i = 0;
+    char *url, *out = malloc(12), *base = "dump_nms";
+    int outfd[128];
+    rtsp_ctrl *ctl;
+    rtp_thread *rtp_th;
+    rtsp_session *sess;
+    rtsp_medium *med;
+    rtp_ssrc *ssrc;
+    rtp_buff conf;
+    rtp_frame fr;
+    nms_rtsp_hints rtsp_hints = { -1 };
 
-	int opt, i = 0;
-	char *url, *out = malloc(12), *base = "dump_nms";
-	int outfd[128];
-	rtsp_ctrl *ctl;
-	rtp_thread *rtp_th;
-	rtsp_session *sess;
-	rtsp_medium *med;
-	rtp_ssrc *ssrc;
-	rtp_buff conf;
-	rtp_frame fr;
-	nms_rtsp_hints rtsp_hints = { -1 };
+    if (argc < 2) {
+        fprintf(stderr, "\tPlease specify at least an url.\n");
+        fprintf(stderr, "\tUsage: %s [-f basename ][-p port] url\n",
+            argv[0]);
+        exit(1);
+    }
 
-	if (argc < 2) {
-		fprintf(stderr, "\tPlease specify at least an url.\n");
-		fprintf(stderr, "\tUsage: %s [-f basename ][-p port] url\n",
-			argv[0]);
-		exit(1);
-	}
+    while ((opt = getopt(argc, argv, "f:p:")) != -1) {
+        switch (opt) {
+            /*  Set output file  */
+        case 'f':
+            base = strdup(optarg);
+            out = realloc(out, strlen(base) + 4);
+            break;
+            /*  Set rtp port  */
+        case 'p':
+            rtsp_hints.first_rtp_port = atoi(optarg);
+            break;
+            /* Unknown option  */
+        case '?':
+            fprintf(stderr, "\n  Unknown option `-%c'.\n", optopt);
+            fprintf(stderr,
+                "\tUsage: %s [-f outputfile ][-p port] url\n",
+                argv[0]);
 
-	while ((opt = getopt(argc, argv, "f:p:")) != -1) {
-		switch (opt) {
-			/*  Set output file  */
-		case 'f':
-			base = strdup(optarg);
-			out = realloc(out, strlen(base) + 4);
-			break;
-			/*  Set rtp port  */
-		case 'p':
-			rtsp_hints.first_rtp_port = atoi(optarg);
-			break;
-			/* Unknown option  */
-		case '?':
-			fprintf(stderr, "\n  Unknown option `-%c'.\n", optopt);
-			fprintf(stderr,
-				"\tUsage: %s [-f outputfile ][-p port] url\n",
-				argv[0]);
+            return 1;
+        }
+    }
 
-			return 1;
-		}
-	}
+    memset(outfd, 0, sizeof(outfd));
 
-	memset(outfd, 0, sizeof(outfd));
+    url = argv[argc - 1];
 
-	url = argv[argc - 1];
+    fprintf(stderr, "URL %s.\n", url);
 
-	fprintf(stderr, "URL %s.\n", url);
+    if ((ctl = rtsp_init(&rtsp_hints)) == NULL) {
+        fprintf(stderr, "Cannot init rtsp.\n");
+        return 1;
+    }
 
-	if ((ctl = rtsp_init(&rtsp_hints)) == NULL) {
-		fprintf(stderr, "Cannot init rtsp.\n");
-		return 1;
-	}
+    if (rtsp_open(ctl, url)) {
+        fprintf(stderr, "rtsp_open failed.\n");
+        // die
+        return 1;
+    }
 
-	if (rtsp_open(ctl, url)) {
-		fprintf(stderr, "rtsp_open failed.\n");
-		// die
-		return 1;
-	}
+    rtsp_wait(ctl);
 
-	rtsp_wait(ctl);
+    //Get the session information
+    sess = ctl->rtsp_queue;
 
-	//Get the session information
-	sess = ctl->rtsp_queue;
+    if (!sess) {
+        fprintf(stderr, "No session available.\n");
+        return 1;
+    }
 
-	if (!sess) {
-		fprintf(stderr, "No session available.\n");
-		return 1;
-	}
+    rtsp_play(ctl, 0.0, 0.0);
 
-	rtsp_play(ctl, 0.0, 0.0);
+    fprintf(stderr, "\nDumping...");
 
-	fprintf(stderr, "\nDumping...");
+    rtp_th = rtsp_get_rtp_th(ctl);
 
-	rtp_th = rtsp_get_rtp_th(ctl);
+    while (!rtp_fill_buffers(rtp_th))    // Till there is something to parse
+    {            // Foreach ssrc active
+        for (ssrc = rtp_active_ssrc_queue(rtsp_get_rtp_queue(ctl));
+             ssrc; ssrc = rtp_next_active_ssrc(ssrc)) {
+            if (!rtp_fill_buffer(ssrc, &fr, &conf)) {    // Parse the stream
 
-	while (!rtp_fill_buffers(rtp_th))	// Till there is something to parse
-	{			// Foreach ssrc active
-		for (ssrc = rtp_active_ssrc_queue(rtsp_get_rtp_queue(ctl));
-		     ssrc; ssrc = rtp_next_active_ssrc(ssrc)) {
-			if (!rtp_fill_buffer(ssrc, &fr, &conf)) {	// Parse the stream
+                if (outfd[fr.pt] ||    // Write it to a file
+                    sprintf(out, "%s.%d", base, fr.pt)
+                    && (outfd[fr.pt] = creat(out, 00644)) > 0) {
+                    if (write(outfd[fr.pt], fr.data, fr.len) < fr.len)
+                        return 1;
+                } else {
+                    return 1;
+                }
+            }
+        }
 
-				if (outfd[fr.pt] ||	// Write it to a file
-				    sprintf(out, "%s.%d", base, fr.pt)
-				    && (outfd[fr.pt] = creat(out, 00644)) > 0) {
-					if (write
-					    (outfd[fr.pt], fr.data, fr.len)
-					    < fr.len)
-						return 1;
-				} else
-					return 1;
-			}
+        switch (i++) {
+        case 0:
+            fprintf(stderr, ".");
+            break;
+        case 1:
+            fprintf(stderr, "o");
+            break;
+        case 2:
+            fprintf(stderr, "O");
+            break;
+        case 3:
+            fprintf(stderr, "o");
+        default:
+            fprintf(stderr, "\r");
+            i = 0;
+        }
+    }
 
-		}
+    for (i = 0; i < 128; i++)
+        if (outfd[i])
+            close(outfd[i]);
 
-		switch (i++) {
-		case 0:
-			fprintf(stderr, ".");
-			break;
-		case 1:
-			fprintf(stderr, "o");
-			break;
-		case 2:
-			fprintf(stderr, "O");
-			break;
-		case 3:
-			fprintf(stderr, "o");
-		default:
-			fprintf(stderr, "\r");
-			i = 0;
-		}
-	}
+    fprintf(stderr, " Complete\n");
 
-	for (i = 0; i < 128; i++)
-		if (outfd[i])
-			close(outfd[i]);
+    rtsp_close(ctl);
 
-	fprintf(stderr, " Complete\n");
+    rtsp_uninit(ctl);
 
-	rtsp_close(ctl);
-
-	rtsp_uninit(ctl);
-
-	return 0;
+    return 0;
 }
