645fac56ba5b2ba4f723a5cc8603eac7fea7367a
Alessandro Molina <amol@cosmo.(none)>
2007-05-23 19:00:42 +0200

    Initial commit

---
+++ include/nemesi/rtspinternals.h
@@ -0,0 +1,223 @@
+/* * 
+ *  This file is part of NeMeSI
+ *
+ *  NeMeSI -- NEtwork MEdia Streamer I
+ *
+ *  Copyright (C) 2006 by
+ *      
+ *      Giampaolo "mancho" Mancini - manchoz@inwind.it
+ *    Francesco "shawill" Varano - shawill@infinto.it
+ *
+ *  NeMeSI is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NeMeSI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with NeMeSI; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ * */
+
+/*! \file rtsp.h
+ * \brief \b rtsp library definitions.
+ * */
+
+/*! \defgroup RTSP RTSP Library
+ *
+ * \brief Real Time Streaming Protocol (RTSP) implementation - rfc 2326.
+ *
+ * The RTSP module requests the media streams and handles the service 
+ * handshake. Once this phase is complete it behaves like a <em>remote 
+ * controller</em> for the requested multimedia stream.
+ *
+ * @{ */
+
+#ifndef __RTSP_H
+#define __RTSP_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+
+#include <nemesi/rtcp.h>
+#include <nemesi/utils.h>
+#include <nemesi/comm.h>
+#include <nemesi/rtsp.h>
+
+#ifndef RTSP_BUFFERSIZE
+#define RTSP_BUFFERSIZE 163840
+#endif
+
+typedef struct nms_rtsp_interleaved_s {
+    int rtp_fd; //!< output rtp local socket 
+    int rtcp_fd; //!< output rtcp local socket
+    union {
+        struct {
+            uint8 rtp_ch;
+            uint8 rtcp_ch;
+        } tcp;
+        struct {
+            uint16 rtp_st;
+            uint16 rtcp_st;
+        } sctp;
+    } proto;
+    struct nms_rtsp_interleaved_s *next;
+} nms_rtsp_interleaved;
+
+
+/*! \enum opcodes: enum which codify the available commands for the user.
+* "COMMAND_NUM" is the total number of recognized commands.
+* "NONE" is a special command, just for internal purpouses
+* and not considered elsewhere.
+*/
+enum opcodes { OPEN, PLAY, PAUSE, STOP, CLOSE, COMMAND_NUM, NONE };
+
+/*!
+ * \brief Struct used for internal comunication
+ *
+ * */
+struct command {
+    enum opcodes opcode;    /*!< Command code inserted by user. */
+    char arg[256];        /*!< Possible command arguments. */
+};
+
+/*!
+ * \brief RTSP Packet buffer.
+ *
+ * There the packets read from the RTSP port are composed and stored.
+ * Since it's possible that the message comes fragmented in many packets
+ * from the underlying transport protocol, we first check for completion and
+ * then the message will be processed.
+ *
+ * */
+struct rtsp_buffer {
+    size_t size;        /*!< Full buffer size. */
+    size_t first_pkt_size;    /*!< First packet size. */
+    char *data;        /*!< Raw data. */
+};
+
+/*!
+ * \brief Main structure for the RTSP module.
+ *
+ * It contains the global data: the current state, the connection port,
+ * the input buffer and the session queue.
+ *
+ * \note The \c type field shows the kind of media stream active, depending
+ * on it RTSP has different behaviours.
+ * 
+ * \see rtsp_session
+ * \see buffer
+ * */
+typedef struct {
+    RTSP_COMMON_IF nms_rtsp_hints *hints;
+    uint16 force_rtp_port;
+    pthread_cond_t cond_busy;
+    nms_transport transport;
+    enum sock_types default_rtp_proto;
+    nms_rtsp_interleaved *interleaved;
+    uint16 next_ilvd_ch;
+    // int fd; /*!< file descriptor for reading the data coming from the server */
+    /*! \enum types enum possible kind of stream. */
+    enum types { M_ON_DEMAND, CONTAINER } type;    /*!< Kind of active
+                               media stream:
+                               Media On Demand or
+                               Container. */
+    char waiting_for[64];    /*!< Expected response from server. */
+    char *server_port;    /*!< Server listening port.
+                 */
+    char *urlname;        /*!< Requested URL */
+    struct rtsp_buffer in_buffer;    /*!< Input buffer. */
+    // rtsp_session *rtsp_queue;/*!< Active sessions. */
+    rtp_thread *rtp_th;
+} rtsp_thread;
+
+    // old init function definitions: -|
+    // /-------------------------------/
+    // |- int init_rtsp(void);
+    // \- struct rtsp_ctrl *init_rtsp(void);
+
+void rtsp_unbusy(rtsp_thread *);
+int rtsp_reinit(rtsp_thread *);
+void rtsp_clean(void *);
+
+/*
+extern int (*cmd[COMMAND_NUM]) (rtsp_thread *, char *);
+*/
+
+extern int (*cmd[COMMAND_NUM]) (rtsp_thread *, ...);
+extern int (*state_machine[STATES_NUM]) (rtsp_thread *, short);
+void *rtsp(void *);
+
+int send_get_request(rtsp_thread *);
+int send_pause_request(rtsp_thread *, char *);
+int send_play_request(rtsp_thread *, char *);
+int send_setup_request(rtsp_thread *);
+int send_teardown_request(rtsp_thread *);
+
+int play_cmd(rtsp_thread *, ...);
+int pause_cmd(rtsp_thread *, ...);
+int stop_cmd(rtsp_thread *, ...);
+int open_cmd(rtsp_thread *, ...);
+int close_cmd(rtsp_thread *, ...);
+
+// RTSP packet handling/creation funcs.
+int seturlname(rtsp_thread *, char *);
+int handle_rtsp_pkt(rtsp_thread *);
+int full_msg_rcvd(rtsp_thread *);
+int rtsp_recv(rtsp_thread *);
+int body_exists(char *);
+int check_response(rtsp_thread *);
+int check_status(char *, rtsp_thread *);
+int set_transport_str(rtp_session *, char **);
+int set_transport_str_udp(rtp_session *, char *);
+int set_transport_str_tcp(rtp_session *, char *);
+#ifdef HAVE_LIBSCTP
+int set_transport_str_sctp(rtp_session *, char *);
+#endif
+int get_transport_str(rtp_session *, char *);
+int get_transport_str_udp(rtp_session *, char *, char *);
+int get_transport_str_tcp(rtp_session *, char *, char *);
+#ifdef HAVE_LIBSCTP
+int get_transport_str_sctp(rtp_session *, char *, char *);
+#endif
+
+#define GCS_INIT 0
+#define GCS_NXT_SESS 1
+#define GCS_NXT_MED 2
+#define GCS_CUR_SESS 3
+#define GCS_CUR_MED 4
+#define GCS_UNINIT 5
+void *get_curr_sess(int cmd, ...);
+// int get_curr_sess(rtsp_thread *, rtsp_session **, rtsp_medium **);
+
+int set_rtsp_sessions(rtsp_thread *, int, char *, char *);
+int set_rtsp_media(rtsp_thread *);
+rtsp_session *rtsp_sess_dup(rtsp_session *);
+rtsp_session *rtsp_sess_create(char *, char *);
+rtsp_medium *rtsp_med_create(int);
+int remove_pkt(rtsp_thread *);
+
+int init_state(rtsp_thread *, short);
+int ready_state(rtsp_thread *, short);
+int playing_state(rtsp_thread *, short);
+int recording_state(rtsp_thread *, short);
+
+int handle_get_response(rtsp_thread *);
+int handle_setup_response(rtsp_thread *);
+int handle_play_response(rtsp_thread *);
+int handle_pause_response(rtsp_thread *);
+int handle_teardown_response(rtsp_thread *);
+
+
+#endif
+/* @} */
--- rtsp/rtsp.c
+++ rtsp/rtsp.c
@@ -1,14 +1,11 @@
 /* * 
- *  $Id:rtsp.c 267 2006-01-12 17:19:45Z shawill $
+ *  $Id$
  *  
  *  This file is part of NeMeSI
  *
  *  NeMeSI -- NEtwork MEdia Streamer I
  *
- *  Copyright (C) 2001 by
- *      
- *      Giampaolo "mancho" Mancini - manchoz@inwind.it
- *    Francesco "shawill" Varano - shawill@infinto.it
+ *  Copyright (C) 2007 by team@streaming.polito.it
  *
  *  NeMeSI is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -26,132 +23,426 @@
  *  
  * */
 
-#include <sys/types.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <nemesi/rtspinternals.h>
+#include <nemesi/utils.h>
+#include <nemesi/methods.h>
+#include <fcntl.h>
 
-#include <nemesi/rtsp.h>
-#include <nemesi/version.h>
+/** @file rtsp.c
+ * This file contains the interface functions to the rtsp packets and requests handling of the library
+ */
 
-#ifndef BUFFERSIZE
-#define BUFFERSIZE 163840
-#endif
-
-int (*cmd[COMMAND_NUM]) (rtsp_thread *, ...);
-int (*state_machine[STATES_NUM]) (rtsp_thread *, short);
+#define RET_ERR(ret_level, ...)    do { \
+                    nms_printf(ret_level, __VA_ARGS__ ); \
+                    free(rtsp_th->comm); \
+                    free(rtsp_th); \
+                    return NULL; \
+                } while (0)
 
 /**
-* Thread rtsp.
-* Waits for commands and handle them
-* It is modeled as a state machine
-* */
-
-void *rtsp(void *rtsp_thrd)
+ * Initialized the library and starts a thread that handles both commands to send
+ * to the server and responses received from the server.
+ *
+ * @hints The ports to use for the new connection. This are just hints.
+ *
+ * @return A rtsp_ctrl instance. This is the structures that control the newly
+ * created communication channel
+ */
+rtsp_ctrl *rtsp_init(nms_rtsp_hints * hints)
 {
-    rtsp_thread *rtsp_th = (rtsp_thread *) rtsp_thrd;
-    struct command *comm = rtsp_th->comm;
-    int command_fd = rtsp_th->pipefd[0];
-    fd_set readset;
-    char ch[1];
-    int n, max_fd;
-    nms_rtsp_interleaved *p;
-    char buffer[BUFFERSIZE];
-#ifdef HAVE_LIBSCTP
-    struct sctp_sndrcvinfo sinfo;
-#endif
+    rtsp_thread *rtsp_th;
+    pthread_attr_t rtsp_attr;
+    pthread_mutexattr_t mutex_attr;
+    // pthread_condattr_t cond_attr;
+    int n;
 
-    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
-    pthread_cleanup_push(rtsp_clean, rtsp_thrd);
-
-    while (1) {
-        FD_ZERO(&readset);
-
-        FD_SET(command_fd, &readset);
-        max_fd = command_fd;
-        if (nmst_is_active(&rtsp_th->transport)) {
-            FD_SET(rtsp_th->transport.fd, &readset);
-            max_fd = max(rtsp_th->transport.fd, max_fd);
-            }
-        for (p = rtsp_th->interleaved; p; p = p->next) {
-            if (p->rtcp_fd >= 0) {
-                FD_SET(p->rtcp_fd, &readset);
-                max_fd = max(p->rtcp_fd, max_fd);
-            }
-        }
-        if (select
-            (max_fd + 1, &readset,
-             NULL, NULL, NULL) < 0) {
-            nms_printf(NMSML_FATAL, "(%s) %s\n", PROG_NAME,
-                   strerror(errno));
-            pthread_exit(NULL);
-        }
-        if (nmst_is_active(&rtsp_th->transport))
-            if (FD_ISSET(rtsp_th->transport.fd, &readset)) {
-                if ((n = rtsp_recv(rtsp_th)) < 0)
-                    pthread_exit(NULL);
-                else if (n == 0) {
-                    nms_printf(NMSML_ERR,
-                           "Server died prematurely!\n");
-                    rtsp_reinit(rtsp_th);
-                    nms_printf(NMSML_NORM,
-                           "Session closed.\n");
-                } else {
-                    while (rtsp_th->in_buffer.size > 0 && full_msg_rcvd(rtsp_th))
-                        if (handle_rtsp_pkt(rtsp_th)) {
-                            // nms_printf(NMSML_ERR, "\nError!\n");
-                            rtsp_reinit(rtsp_th);
-                        }
-                } 
-            }
-
-        for (p = rtsp_th->interleaved; p; p = p->next) {
-            if (p->rtcp_fd >= 0 && FD_ISSET(p->rtcp_fd, &readset)) {
-                switch (rtsp_th->transport.type) {
-                case TCP:
-                    n = recv(p->rtcp_fd, buffer+4, BUFFERSIZE-4, 0);
-                    buffer[0]='$';
-                    buffer[1]= p->proto.tcp.rtcp_ch;
-                    *((uint16 *) &buffer[2]) = htons((uint16) n);
-                    nmst_write(&rtsp_th->transport, buffer, n+4, NULL);
-                    nms_printf(NMSML_DBG2,
-                           "Sent RTCP packet on channel %u.\n",
-                           buffer[1]);
-                    break;
-#ifdef HAVE_LIBSCTP
-                case SCTP:
-                    n = recv(p->rtcp_fd, buffer, BUFFERSIZE, 0);
-                    memset(&sinfo, 0, sizeof(sinfo));
-                    sinfo.sinfo_stream = p->proto.sctp.rtcp_st;
-                    sinfo.sinfo_flags = SCTP_UNORDERED;
-                    nmst_write(&rtsp_th->transport, buffer, n, &sinfo);
-                    nms_printf(NMSML_DBG2,
-                           "Sent RTCP packet on stream %u.\n",
-                           sinfo.sinfo_stream);
-                    break;
-#endif
-                default:
-                    recv(p->rtcp_fd, buffer, BUFFERSIZE, 0);
-                    nms_printf(NMSML_DBG2,
-                       "Unable to send RTCP interleaved packet.\n");
-                    break;
-                }
-            }
-        }
-
-        if (FD_ISSET(command_fd, &readset)) {
-            pthread_mutex_lock(&(rtsp_th->comm_mutex));
-            read(command_fd, ch, 1);
-            if (cmd[comm->opcode] (rtsp_th, comm->arg)) {
-                nms_printf(NMSML_DBG3,
-                       "Error handling user command.\n\n");
-                rtsp_th->busy = 0;
-            }
-            rtsp_th->comm->opcode = NONE;
-            pthread_mutex_unlock(&(rtsp_th->comm_mutex));
-        }
+    // if ( !(rtsp_th = (rtsp_thread *) malloc(sizeof(rtsp_thread))) )
+    // We use calloc so that we are not in need to initialize to zero below
+    if (!(rtsp_th = (rtsp_thread *) calloc(1, sizeof(rtsp_thread)))) {
+        nms_printf(NMSML_FATAL, "Could not alloc memory!\n");
+        return NULL;
     }
 
-    pthread_cleanup_pop(1);
-/*    return NULL; */
+    /* not needed if we use calloc
+     * rtsp_th->comm = NULL;
+     */
+
+    if (pipe(rtsp_th->pipefd) < 0)
+        RET_ERR(NMSML_FATAL, "Could not create pipe\n");
+
+    if ((n = pthread_mutexattr_init(&mutex_attr)) > 0)
+        RET_ERR(NMSML_FATAL, "Could not init mutex attributes\n");
+
+#if 0
+#ifdef    _POSIX_THREAD_PROCESS_SHARED
+    if ((n =
+         pthread_mutexattr_setpshared(&mutex_attr,
+                      PTHREAD_PROCESS_SHARED)) > 0)
+        return NULL;
+#endif
+#endif
+    if ((n = pthread_mutex_init(&(rtsp_th->comm_mutex), &mutex_attr)) > 0)
+        RET_ERR(NMSML_FATAL, "Could not init mutex\n");
+
+    /* // we give NULL to cond_init: uncommet this (and the declaration above)
+     * if you want to give different attributes to cond
+     if (pthread_condattr_init(&cond_attr) > 0)
+     RET_ERR(NMSML_FATAL, "Could not init condition variable attributes\n");
+     */
+    if (pthread_cond_init(&(rtsp_th->cond_busy), NULL /*&cond_attr */ ) >
+        0)
+        RET_ERR(NMSML_FATAL, "Could not init condition variable\n");
+
+    if ((rtsp_th->comm =
+         (struct command *) malloc(sizeof(struct command))) == NULL)
+        RET_ERR(NMSML_FATAL, "Could not alloc memory\n");
+
+    nmst_init(&rtsp_th->transport);
+    rtsp_th->default_rtp_proto = UDP;
+    rtsp_th->status = INIT;
+#if 0                // we do not need to initilize to zero because of calloc usage
+    rtsp_th->descr_fmt = 0;
+    memset(rtsp_th->waiting_for, '\0', sizeof(rtsp_th->waiting_for));
+    rtsp_th->busy = 0;
+    rtsp_th->urlname = NULL;
+    (rtsp_th->in_buffer).size = 0;
+    (rtsp_th->in_buffer).first_pkt_size = 0;
+    (rtsp_th->in_buffer).data = NULL;
+    rtsp_th->rtsp_queue = NULL;
+    rtsp_th->force_rtp_port = 0;
+#endif
+    CC_ACCEPT_ALL(rtsp_th->accepted_CC);
+
+    rtsp_th->hints = hints;
+    // check for the exactness of values hinted
+    if (hints) {        // hints given
+        // set first RTP port
+        if (hints->first_rtp_port > 0) {
+            if (hints->first_rtp_port < RTSP_MIN_RTP_PORT)
+                RET_ERR(NMSML_ERR,
+                    "For security reasons RTSP Library imposes that port number should be greater than %d\n",
+                    RTSP_MIN_RTP_PORT);
+            else if (hints->first_rtp_port > 65535)
+                RET_ERR(NMSML_ERR,
+                    "Port number can't be greater than 65535\n");
+            rtsp_th->force_rtp_port = hints->first_rtp_port;
+            nms_printf(NMSML_WARN,
+                   "RTP ports forced by user (not randomly generated)\n");
+        }
+        //force RTSP protocol
+        switch (hints->pref_rtsp_proto) {
+        case SOCK_NONE:
+        case TCP:
+            rtsp_th->transport.type = TCP;
+            break;
+#ifdef HAVE_LIBSCTP
+        case SCTP:
+            rtsp_th->transport.type = SCTP;
+            break;
+#endif
+        default:
+            RET_ERR(NMSML_ERR, "RTSP protocol not supported!\n");
+        }
+        //force RTP Protocol
+        switch (hints->pref_rtp_proto) {
+        case SOCK_NONE:
+        case UDP:
+            rtsp_th->default_rtp_proto = UDP;
+            break;
+        case TCP:
+            if (rtsp_th->transport.type == TCP)
+                rtsp_th->default_rtp_proto = TCP;
+            else
+                RET_ERR(NMSML_ERR, "RTP/RTSP protocols combination not supported!\n");
+            break;
+#ifdef HAVE_LIBSCTP
+        case SCTP:
+            if (rtsp_th->transport.type == SCTP)
+                rtsp_th->default_rtp_proto = SCTP;
+            else
+                RET_ERR(NMSML_ERR, "RTP/RTSP protocols combination not supported!\n");
+            break;
+#endif
+        default:
+            RET_ERR(NMSML_ERR, "RTP protocol not supported!\n");
+        }
+    }
+    // hook to rtp lib
+    if (!(rtsp_th->rtp_th = rtp_init()))
+        RET_ERR(NMSML_ERR, "Cannot initialize RTP structs\n");
+
+    cmd[0] = open_cmd;
+    cmd[1] = play_cmd;
+    cmd[2] = pause_cmd;
+    cmd[3] = stop_cmd;
+    cmd[4] = close_cmd;
+
+    state_machine[0] = init_state;
+    state_machine[1] = ready_state;
+    state_machine[2] = playing_state;
+    state_machine[3] = recording_state;
+
+    // Creation of RTSP Thread
+    pthread_attr_init(&rtsp_attr);
+    if (pthread_attr_setdetachstate(&rtsp_attr, PTHREAD_CREATE_JOINABLE) !=
+        0)
+        RET_ERR(NMSML_FATAL, "Cannot set RTSP Thread attributes!\n");
+
+    if ((n =
+         pthread_create(&rtsp_th->rtsp_tid, NULL, &rtsp,
+                (void *) rtsp_th)) > 0)
+        RET_ERR(NMSML_FATAL, "Cannot create RTSP Thread: %s\n",
+            strerror(n));
+
+    return (rtsp_ctrl *) rtsp_th;
+}
+
+/**
+ * Sends to the rtsp main loop the request to close the connection for the given control structure
+ * @rtsp_ctl The control structure for which to close the connection
+ * @return 0
+ */
+int rtsp_close(rtsp_ctrl * rtsp_ctl)
+{
+    pthread_mutex_lock(&(rtsp_ctl->comm_mutex));
+    rtsp_ctl->comm->opcode = CLOSE;
+    write(rtsp_ctl->pipefd[1], "c", 1);
+    rtsp_ctl->busy = 1;
+    pthread_mutex_unlock(&(rtsp_ctl->comm_mutex));
+
+    return 0;
+}
+
+/**
+ * Waits for the main loop to handle the last given command, this should be called after issuing a command
+ * to the rtsp main loop.
+ * @rtsp_ctl The control structure for which to wait.
+ */
+void rtsp_wait(rtsp_ctrl * rtsp_ctl)
+{
+    rtsp_thread *rtsp_th = (rtsp_thread *) rtsp_ctl;
+
+    pthread_mutex_lock(&(rtsp_th->comm_mutex));
+    while (rtsp_th->busy)
+        pthread_cond_wait(&(rtsp_th->cond_busy),
+                  &(rtsp_th->comm_mutex));
+
+    pthread_mutex_unlock(&(rtsp_th->comm_mutex));
+}
+
+/**
+ * Gets the head of the RTP sessions queue linked to the given RTSP controller
+ * @rtsp_ctl The RTSP controller for which to get the RTP sessions queue
+ * @return The RTP session queue
+ */
+inline rtp_session *rtsp_get_rtp_queue(rtsp_ctrl * rtsp_ctl)
+{
+    return ((rtsp_thread *) rtsp_ctl)->rtp_th->rtp_sess_head;
+}
+
+/**
+ * Gets the RTP thread linked to the given RTSP controller
+ * @rtsp_ctl The RTSP controller for which to get the RTP thread
+ * @return The RTP thread
+ */
+inline rtp_thread *rtsp_get_rtp_th(rtsp_ctrl * rtsp_ctl)
+{
+    return ((rtsp_thread *) rtsp_ctl)->rtp_th;
+}
+
+/**
+ * Prints the informations about the given RTSP controller: 
+ * Session informations, medium informations.
+ * @rtsp_ctl The controller for which to print the informations
+ */
+void rtsp_info_print(rtsp_ctrl * rtsp_ctl)
+{
+    // tmp
+    rtsp_thread *rtsp_th = (rtsp_thread *) rtsp_ctl;
+    rtsp_session *sess;
+    rtsp_medium *med;
+    char **str;
+    // struct attr *attr;
+    sdp_attr *attr;
+
+    char *sdes[ /*13 */ ] = { SDP_SESSION_FIELDS };
+    char *mdes[ /*5 */ ] = { SDP_MEDIA_FIELDS };
+
+    sess = rtsp_th->rtsp_queue;
+
+    nms_printf(NMSML_NORM, BLANK_LINE);
+
+    if (!sess) {
+        nms_printf(NMSML_NORM, "No Connection!\n\n");
+        return;
+    }
+
+    while (sess) {
+        med = sess->media_queue;
+        nms_printf(NMSML_NORM, "---- RTSP Session Infos: %s ----\n",
+               sess->pathname);
+        for (str = (char **) (sess->info);
+             (void*)str < (void*) &(sess->info->attr_list); str++)
+            if (*str)
+                nms_printf(NMSML_ALWAYS, "* %s: %s\n",
+                       sdes[str - (char **) (sess->info)],
+                       *str);
+        for (attr = sess->info->attr_list; attr; attr = attr->next)
+            nms_printf(NMSML_ALWAYS, "%s\n", attr->a);
+        while (med) {
+            nms_printf(NMSML_NORM,
+                   "\n\t---- RTSP Medium Infos: %s ----\n",
+                   med->filename);
+            for (str = (char **) (med->medium_info);
+                 (void*)str < (void*) &(med->medium_info->attr_list);
+                 str++)
+                if (*str)
+                    nms_printf(NMSML_ALWAYS,
+                           "\t* %s: %s\n",
+                           mdes[str -
+                            (char **) (med->
+                                   medium_info)],
+                           *str);
+            for (attr = med->medium_info->attr_list; attr;
+                 attr = attr->next)
+                nms_printf(NMSML_ALWAYS, "\t* %s\n", attr->a);
+            med = med->next;
+        }
+        sess = sess->next;
+    }
+    nms_printf(NMSML_ALWAYS, "\n");
+
+}
+
+/**
+ * Checks if the given controller is waiting to process a given command
+ * @rtsp_ctl The controller for which to check the state
+ * @return TRUE if its busy, FALSE if its ready
+ */
+inline int rtsp_is_busy(rtsp_ctrl * rtsp_ctl)
+{
+    return rtsp_ctl->busy;
+}
+
+/**
+ * Sends to the controller the request to open a given url
+ * @rtsp_ctl The controller that should open the url
+ * @urlanem The path of the document to open
+ * @return 0
+ */
+int rtsp_open(rtsp_ctrl * rtsp_ctl, char *urlname)
+{
+    if (!urlname || !*urlname)
+        return 1;
+
+    pthread_mutex_lock(&(rtsp_ctl->comm_mutex));
+    rtsp_ctl->comm->opcode = OPEN;
+    strncpy(rtsp_ctl->comm->arg, urlname, sizeof(rtsp_ctl->comm->arg));
+    write(rtsp_ctl->pipefd[1], "o", 1);
+    rtsp_ctl->busy = 1;
+    pthread_mutex_unlock(&(rtsp_ctl->comm_mutex));
+
+    return 0;
+}
+
+/**
+ * Sends to the controller the request to pause the current stream
+ * @rtsp_ctl The controller for which to pause the content
+ * @return 0
+ */
+int rtsp_pause(rtsp_ctrl * rtsp_ctl)
+{
+
+    pthread_mutex_lock(&(rtsp_ctl->comm_mutex));
+    rtsp_ctl->comm->opcode = PAUSE;
+    write(rtsp_ctl->pipefd[1], "z", 1);
+    *(rtsp_ctl->comm->arg) = '\0';
+    rtsp_ctl->busy = 1;
+    pthread_mutex_unlock(&(rtsp_ctl->comm_mutex));
+
+    return 0;
+}
+
+/**
+ * Sends to the controller the request to play the content in a given range
+ * @rtsp_ctl The controller that should start playing its content
+ * @start from where to start playing
+ * @stop where to stop playing
+ * @return 0
+ */
+int rtsp_play(rtsp_ctrl * rtsp_ctl, double start, double stop)
+{
+    pthread_mutex_lock(&(rtsp_ctl->comm_mutex));
+    rtsp_ctl->comm->opcode = PLAY;
+    if ((start >= 0) && (stop > 0))
+        sprintf(rtsp_ctl->comm->arg, "npt=%.2f-%.2f", start, stop);
+    else if (start >= 0)
+        sprintf(rtsp_ctl->comm->arg, "npt=%.2f-", start);
+    else if (stop > 0)
+        sprintf(rtsp_ctl->comm->arg, "npt=-%.2f", stop);
+    else
+        *(rtsp_ctl->comm->arg) = '\0';
+
+    write(rtsp_ctl->pipefd[1], "p", 1);
+    rtsp_ctl->busy = 1;
+    pthread_mutex_unlock(&(rtsp_ctl->comm_mutex));
+
+    return 0;
+}
+
+/**
+ * Sends to the controller the request to open a given url
+ * @rtsp_ctl The controller that should open the url
+ * @urlanem The path of the document to open
+ * @return 0
+ */
+int rtsp_stop(rtsp_ctrl * rtsp_ctl)
+{
+
+    pthread_mutex_lock(&(rtsp_ctl->comm_mutex));
+    rtsp_ctl->comm->opcode = STOP;
+    write(rtsp_ctl->pipefd[1], "s", 1);
+    *(rtsp_ctl->comm->arg) = '\0';
+    rtsp_ctl->busy = 1;
+    pthread_mutex_unlock(&(rtsp_ctl->comm_mutex));
+
+    return 0;
+}
+
+/**
+ * Uninits the given RTSP controller
+ * @rtsp_ctl The controller to shut down
+ * @return 0 If Shutdown was done
+ * @return 1 If it wasn't possible to cancel the running main loop
+ */
+int rtsp_uninit(rtsp_ctrl * rtsp_ctl)
+{
+    void *ret = NULL;
+
+    /* THREAD CANCEL */
+    nms_printf(NMSML_DBG1, "Sending cancel signal to all threads\n");
+    if (rtsp_ctl->rtsp_tid > 0) {
+        nms_printf(NMSML_DBG1,
+               "Sending cancel signal to RTSP Thread (ID: %lu)\n",
+               rtsp_ctl->rtsp_tid);
+        if (pthread_cancel(rtsp_ctl->rtsp_tid) != 0)
+            nms_printf(NMSML_DBG2,
+                   "Error while sending cancelation to RTSP Thread.\n");
+        else
+            pthread_join(rtsp_ctl->rtsp_tid, (void **) &ret);
+        if (ret != PTHREAD_CANCELED) {
+            nms_printf(NMSML_DBG2,
+                   "Warning! RTSP Thread joined, but  not canceled!\n");
+            return 1;
+        }
+    } else {
+        nms_printf(NMSML_DBG1,
+               "Cannot send cancel signal to RTSP Thread\n");
+        return 1;
+    }
+
+    free(rtsp_ctl->comm);
+    free(rtsp_ctl);
+
+    return 0;
 }
