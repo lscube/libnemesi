54a4a5f9dac891fa38b59bbba1ecfcb7d80c812f
Luca Barbato <lu_zero@gentoo.org>
2007-06-28 22:32:36 +0200

    GPL -> LGPL

--- include/nemesi/sdp.h
+++ include/nemesi/sdp.h
@@ -1,28 +1,22 @@
 /* * 
- *  $Id$
- *  
- *  This file is part of NeMeSI
+ * This file is part of NetEmbryo
  *
- *  NeMeSI -- NEtwork MEdia Streamer I
+ * Copyright (C) 2007 by LScube team <team@streaming.polito.it>
+ * See AUTHORS for more details
+ * 
+ * NetEmbryo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- *  Copyright (C) 2006 by
- *      
- *      Giampaolo "mancho" Mancini - giampaolo.mancini@polito.it
- *    Francesco "shawill" Varano - francesco.varano@polito.it
+ * NetEmbryo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  NeMeSI is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  NeMeSI is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with NeMeSI; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NetEmbryo; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *  
  * */
 
