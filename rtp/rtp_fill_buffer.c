c50d3d9358ff95fc1421cdd6d93c48cbc7659b0b
Luca Barbato <lu_zero@gentoo.org>
2007-09-10 18:19:00 +0200

    Cosmetics

--- rtp/rtp_fill_buffer.c
+++ rtp/rtp_fill_buffer.c
@@ -36,7 +36,6 @@ int rtp_fill_buffer(rtp_ssrc * stm_src, rtp_frame * fr, rtp_buff * config)
 {
     rtp_pkt *pkt;
     int err;
-
     if (!(pkt = rtp_get_pkt(stm_src, NULL))) {
         usleep(0);
         return RTP_BUFF_EMPTY;
@@ -50,7 +49,8 @@ int rtp_fill_buffer(rtp_ssrc * stm_src, rtp_frame * fr, rtp_buff * config)
     fr->fps = stm_src->rtp_sess->ptdefs[fr->pt]->fps = 
         (double) stm_src->rtp_sess->ptdefs[pkt->pt]->rate/
             abs(fr->timestamp - stm_src->ssrc_stats.lastts);
-    stm_src->rtp_sess->ptdefs[fr->pt]->prev_timestamp = stm_src->ssrc_stats.lastts = fr->timestamp;
+    stm_src->rtp_sess->ptdefs[fr->pt]->prev_timestamp =
+        stm_src->ssrc_stats.lastts = fr->timestamp;
 
     while ((err = stm_src->rtp_sess->parsers[fr->pt] (stm_src, fr, config)) 
             == EAGAIN);
