45a2c7d0c460ec1cbddb0bda42e0bd9ff081f3d4
Alessandro Molina <amol@cosmo.(none)>
2007-09-13 11:42:11 +0200

    Removed forgotten printf

--- bufferpool/podel.c
+++ bufferpool/podel.c
@@ -41,8 +41,6 @@
 * */
 int podel(playout_buff * po, int index)
 {
-
-    fprintf(stderr, "DEL PO\n");
     pthread_mutex_lock(&(po->po_mutex));
 
     if (po->pobuff[index].next != -1)
@@ -59,7 +57,6 @@ int podel(playout_buff * po, int index)
     po->pocount--;
 
     pthread_mutex_unlock(&(po->po_mutex));
-    fprintf(stderr, "UNDEL PO\n");
 
     return 0;
 }
