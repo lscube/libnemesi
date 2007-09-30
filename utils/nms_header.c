f64df506068a75a9ff06c6e2e1e4c3635245908f
Luca Barbato <lu_zero@gentoo.org>
2007-09-30 13:11:44 +0200

    Cleaning up for release

--- utils/nms_header.c
+++ utils/nms_header.c
@@ -27,17 +27,7 @@
 
 inline void nms_header(void)
 {
-    char svnrev[32] = "";
-#ifdef SVNREV
-    char *tkn;
-
-    // strcat(svnrev, SVNREV+1);
-    snprintf(svnrev, sizeof(svnrev), "- SVN %s", SVNREV + 1);
-    if ((tkn = strchr(svnrev, '$')))
-        *tkn = '\0';
-#endif
     nms_printf(NMSML_ALWAYS,
-           "\n" NMSCLR_BLUE_BOLD "%s - %s -- release %s %s(%s)\n\n"
-           NMSCLR_DEFAULT, PROG_NAME, PROG_DESCR, VERSION, svnrev,
-           VERSION_NAME);
+           "\n" NMSCLR_BLUE_BOLD "%s - %s -- release %s (%s)\n\n"
+           NMSCLR_DEFAULT, PROG_NAME, PROG_DESCR, VERSION, VERSION_NAME);
 }
