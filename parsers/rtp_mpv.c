047f2c68c65b0e4bf6342ada6e87d136eca9b909
Dario Gallucci <dario.gallucci@polito.it>
2007-11-04 20:43:35 +0100

    Convert mpv parser from do...while loop to EAGAIN

--- parsers/rtp_mpv.c
+++ parsers/rtp_mpv.c
@@ -31,12 +31,11 @@ static rtpparser_info mpv_served = {
     {"MPV", NULL}
 };
 
-#define DEFAULT_MPV_DATA_FRAME 65535
-
 typedef struct {
-//      mpa_data mpa_info;
-    char *data;
-    uint32_t data_size;
+    char *data;     //!< constructed frame, fragments will be copied there
+    long len;       //!< buf length, it's the sum of the fragments length
+    long data_size; //!< allocated bytes for data
+    long timestamp; //!< timestamp of progressive frame
 } rtp_mpv;
 
 typedef struct {
@@ -89,76 +88,87 @@ typedef struct {
 #define RTP_MPV_TR(pkt)            (RTP_MPV_PKT(pkt)->tr_h << 8 | RTP_MPV_PKT(pkt)->tr_l)
 #endif
 
-static int mpv_parse(rtp_ssrc * stm_src, rtp_frame * fr, rtp_buff * config)
+static int mpv_init_parser(rtp_session * rtp_sess, unsigned pt)
 {
-    rtp_mpv *mpv_priv = stm_src->privs[fr->pt];
+    rtp_mpv *priv = calloc(1, sizeof(rtp_mpv));
+
+    if (!priv)
+        return RTP_ERRALLOC;
+
+    rtp_sess->ptdefs[pt]->priv = priv;
+
+    return 0;
+}
+
+static int mpv_uninit_parser(rtp_ssrc * ssrc, unsigned pt)
+{
+    rtp_mpv *priv = ssrc->rtp_sess->ptdefs[pt]->priv;
+
+    if (priv && priv->data)
+        free(priv->data);
+    if (priv)
+        free(priv);
+
+    ssrc->rtp_sess->ptdefs[pt]->priv = NULL;
+
+    return 0;
+}
+
+static int mpv_parse(rtp_ssrc * ssrc, rtp_frame * fr, rtp_buff * config)
+{
+    rtp_mpv *priv = ssrc->rtp_sess->ptdefs[fr->pt]->priv;
     rtp_pkt *pkt;
     size_t pkt_len;
-    uint32_t tot_pkts = 0;
+    int err = RTP_FILL_OK;
 
-    if (!(pkt = rtp_get_pkt(stm_src, &pkt_len)))
+    if (!(pkt = rtp_get_pkt(ssrc, &pkt_len)))
         return RTP_BUFF_EMPTY;
 
-    // pkt_pt = RTP_PKT_PT(pkt);
-    // fr->timestamp = RTP_PKT_TS(pkt);
-
-
-    nms_printf(NMSML_DBG3, "\n[MPV]: header: mbz:%u t:%u tr:%u an:%u n:%u s:%u b:%u e:%u p:%u fbv:%u bfc:%u ffv:%u ffc:%u\n", RTP_MPV_PKT(pkt)->mbz, RTP_MPV_PKT(pkt)->t,    /*ntohs( */
-           RTP_MPV_TR(pkt) /*) */ ,
+    nms_printf(NMSML_DBG3, "\n[MPV]: header: mbz:%u t:%u tr:%u an:%u n:%u s:%u b:%u e:%u p:%u fbv:%u bfc:%u ffv:%u ffc:%u\n", RTP_MPV_PKT(pkt)->mbz, RTP_MPV_PKT(pkt)->t,
+           RTP_MPV_TR(pkt),
            RTP_MPV_PKT(pkt)->an, RTP_MPV_PKT(pkt)->n,
            RTP_MPV_PKT(pkt)->s, RTP_MPV_PKT(pkt)->b,
            RTP_MPV_PKT(pkt)->e, RTP_MPV_PKT(pkt)->p,
            RTP_MPV_PKT(pkt)->fbv, RTP_MPV_PKT(pkt)->bfc,
            RTP_MPV_PKT(pkt)->ffv, RTP_MPV_PKT(pkt)->ffc);
-#if 1
-    // discard pkt if it's fragmented and the first fragment was lost
-    while (!RTP_MPV_PKT(pkt)->b) {
-        rtp_rm_pkt(stm_src);
-        if (!(pkt = rtp_get_pkt(stm_src, &pkt_len)))
-            return RTP_BUFF_EMPTY;
-        else if (RTP_PKT_PT(pkt) != fr->pt)
-            return RTP_PARSE_ERROR;
+
+    if (priv->len && (RTP_PKT_TS(pkt) != priv->timestamp)) {
+        //incomplete packet without final fragment
+        priv->len = 0;
+        return RTP_PKT_UNKNOWN;
     }
-#endif
 
-    /* XXX if the frame is not fragmented we could use directly the data contained in bufferpool 
-     * instead of memcpy the frame in a newly allocated space */
-    // init private struct if this is the first time we're called
-    if (!mpv_priv) {
-        nms_printf(NMSML_DBG3,
-               "[rtp_mpv] allocating new private struct...");
-        if (!
-            (stm_src->privs[fr->pt] = mpv_priv =
-             malloc(sizeof(rtp_mpv))))
-            return RTP_ERRALLOC;
-        mpv_priv->data_size = max(DEFAULT_MPV_DATA_FRAME, pkt_len);
-        if (!(fr->data = mpv_priv->data = malloc(mpv_priv->data_size)))
-            return RTP_ERRALLOC;
-        nms_printf(NMSML_DBG3, "done\n");
-    } else
-        fr->data = mpv_priv->data;
+    // discard pkt if it's fragmented and the first fragment was lost
+    if (!priv->len && !RTP_MPV_PKT(pkt)->b) {
+        rtp_rm_pkt(ssrc);
+        return RTP_PKT_UNKNOWN;
+    }
 
-    do {
-        pkt_len = RTP_MPV_DATA_LEN(pkt, pkt_len);
-        if (mpv_priv->data_size < tot_pkts + pkt_len) {
-            nms_printf(NMSML_DBG3,
-                   "[rtp_mpv] reallocating data...");
-            if ((fr->data = mpv_priv->data =
-                 realloc(mpv_priv->data, tot_pkts + pkt_len)))
-                return RTP_ERRALLOC;
-            nms_printf(NMSML_DBG3, "done\n");
+    pkt_len = RTP_MPV_DATA_LEN(pkt, pkt_len);
+
+    if (priv->data_size < pkt_len + priv->len) {
+        if (!(priv->data = realloc(priv->data, pkt_len + priv->len))) {
+            return RTP_ERRALLOC;
         }
-        memcpy(fr->data + tot_pkts, RTP_MPV_DATA(pkt), pkt_len);
-        tot_pkts += pkt_len;
-        rtp_rm_pkt(stm_src);
-    } while (!RTP_PKT_MARK(pkt) && (pkt = rtp_get_pkt(stm_src, &pkt_len))
-         && (RTP_PKT_TS(pkt) == fr->timestamp)
-         && (RTP_PKT_PT(pkt) == fr->pt));
+        priv->data_size = pkt_len + priv->len;
+    }
 
-    fr->len = tot_pkts;
+    memcpy(priv->data + priv->len, RTP_MPV_DATA(pkt), pkt_len);
+    priv->len += pkt_len;
+
+    if (!RTP_PKT_MARK(pkt)) {
+        priv->timestamp = RTP_PKT_TS(pkt);
+        err = EAGAIN;
+    } else {
+        fr->data = priv->data;
+        fr->len  = priv->len;
+        priv->len = 0;
+    }
+
     nms_printf(NMSML_DBG3, "fr->len: %d\n", fr->len);
 
-    return RTP_FILL_OK;
+    rtp_rm_pkt(ssrc);
+    return err;
 }
 
-RTP_PARSER(mpv);
+RTP_PARSER_FULL(mpv);
