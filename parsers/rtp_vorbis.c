3ded6af0e7585a9f6a2485d7ac64ff50b9a6d3bd
Luca Barbato <lu_zero@gentoo.org>
2007-11-13 17:10:10 +0100

    Cleanup vorbis part I: remove nonstandard timestamps

--- parsers/rtp_vorbis.c
+++ parsers/rtp_vorbis.c
@@ -40,254 +40,15 @@ typedef struct {
     int pkts;       //!< number of packets yet to process in the aggregate
     uint8_t *buf;   //!< constructed frame, fragments will be copied there 
     long len;       //!< buf length, it's the sum of the fragments length
-    long timestamp; //!< calculated timestamp
     int id;         //!< Vorbis id, it could change across packets.
     rtp_xiph_conf *conf;        //!< configuration list
     int conf_len;
-    int modes;                  //!< Internal vorbis data
-    long blocksizes[2];         //!< Internal vorbis data
-    int param_blockflag[64];    //!< Internal vorbis data
-    long prev_bs;               //!< Internal vorbis data
-    long curr_bs;               //!< Internal vorbis data
 } rtp_vorbis;
 
 static rtpparser_info vorbis_served = {
     -1,
     {"vorbis", NULL}
 };
-
-//helpers
-static int rtp_ilog(unsigned int v)
-{
-    int ret = 0;
-    while (v) {
-        ret++;
-        v >>= 1;
-    }
-    return (ret);
-}
-
-static int icount(unsigned int v)
-{
-    int ret = 0;
-    while (v) {
-        ret += v & 1;
-        v >>= 1;
-    }
-    return (ret);
-}
-
-static long maptype_quantvals(int entries, int dim)
-{
-    long vals = floor(pow((float) entries, 1.f / dim));
-
-    /* the above *should* be reliable, but we'll not assume that FP is
-       ever reliable when bitstream sync is at stake; verify via integer
-       means that vals really is the greatest value of dim for which
-       vals^b->bim <= b->entries */
-    /* treat the above as an initial guess */
-    while (1) {
-        long acc = 1;
-        long acc1 = 1;
-        int i;
-        for (i = 0; i < dim; i++) {
-            acc *= vals;
-            acc1 *= vals + 1;
-        }
-        if (acc <= entries && acc1 > entries) {
-            return (vals);
-        } else {
-            if (acc > entries) {
-                vals--;
-            } else {
-                vals++;
-            }
-        }
-    }
-}
-
-static int cfg_parse(rtp_vorbis * vorb, uint8_t *data, int len, int off) 
-{
-    bit_context opb;
-    int num, i, j, k = -1, channels;
-
-    //id packet
-    vorb->blocksizes[0] = 1 << (data[28] & 0x0f);
-    vorb->blocksizes[1] = 1 << (data[28] >> 4);
-    channels = data[11];
-    //cb packet
-    bit_readinit(&opb, data + off, len - off);
-    if (5 != bit_read(&opb, 8))
-        return RTP_PARSE_ERROR;
-
-    for (i = 6; i > 0; i--)
-        bit_read(&opb, 8);
-
-    num = bit_read(&opb, 8) + 1;
-
-    for (; num > 0; num--) {
-        int entries, quantvals = -1, maptype, q_quant, dim;
-        bit_read(&opb, 24);    //magic
-        dim = bit_read(&opb, 16);    //dimensions
-        entries = bit_read(&opb, 24);    //entries
-
-        switch (bit_read(&opb, 1)) {
-        case 0:    //unordered
-            if (bit_read(&opb, 1)) {
-                for (i = 0; i < entries; i++)
-                    if (bit_read(&opb, 1))
-                        bit_read(&opb, 5);
-            } else
-                for (i = 0; i < entries; i++)
-                    bit_read(&opb, 5);
-            break;
-        case 1:    //ordered
-            bit_read(&opb, 5);    //len
-            for (i = 0; i < entries;)
-                i += bit_read(&opb, rtp_ilog(entries - i));
-            break;
-        }
-        switch ((maptype = bit_read(&opb, 4))) {
-        case 0:
-            break;
-
-        case 1:
-        case 2:
-            bit_read(&opb, 32);
-            bit_read(&opb, 32);
-            q_quant = bit_read(&opb, 4) + 1;
-            bit_read(&opb, 1);
-
-            switch (maptype) {
-            case 1:
-                quantvals = maptype_quantvals(entries, dim);
-                break;
-
-            case 2:
-                quantvals = entries * dim;
-                break;
-            }
-
-            for (i = 0; i < quantvals; i++)
-                bit_read(&opb, q_quant);
-            break;
-        }
-
-    }
-
-    //times
-    num = bit_read(&opb, 6) + 1;
-    for (; num > 0; num--)
-        bit_read(&opb, 16);
-
-    //floors
-    num = bit_read(&opb, 6) + 1;
-    for (; num > 0; num--)
-        switch (bit_read(&opb, 16)) {
-
-        case 0:
-            bit_read(&opb, 16);
-            bit_read(&opb, 16);
-            bit_read(&opb, 16);
-            bit_read(&opb, 6);
-            bit_read(&opb, 8);
-            for (i = bit_read(&opb, 4) + 1; i > 0; i--)
-                bit_read(&opb, 8);
-            break;
-        case 1:
-            {
-                int pclass[31], pdim[16], rangebits,
-                    partitions = bit_read(&opb, 5);
-                for (i = 0; i < partitions; i++) {
-                    pclass[i] = bit_read(&opb, 4);
-                    if (k < pclass[i])
-                        k = pclass[i];
-                }
-                for (j = 0; j < k + 1; j++) {
-                    int subs;
-                    pdim[j] = bit_read(&opb, 3) + 1;    // dim
-                    subs = bit_read(&opb, 2);
-                    if (subs)
-                        bit_read(&opb, 8);    // book
-                    for (i = 0; i < (1 << subs); i++)
-                        bit_read(&opb, 8);
-                }
-
-                bit_read(&opb, 2);
-                rangebits = bit_read(&opb, 4);
-                for (j = 0, k = 0; j < partitions; j++) {
-                    bit_read(&opb,
-                         rangebits * pdim[pclass[j]]);
-                }
-            }
-        }
-
-    //residues
-    num = bit_read(&opb, 6) + 1;
-    for (; num > 0; num--) {
-        int partitions, acc = 0;
-        bit_read(&opb, 16);    // unpack is always the same
-        bit_read(&opb, 24);    // begin
-        bit_read(&opb, 24);    // end
-        bit_read(&opb, 24);    // grouping
-        partitions = bit_read(&opb, 6) + 1;
-        bit_read(&opb, 8);    // groupbook
-        for (j = 0; j < partitions; j++) {
-            int cascade = bit_read(&opb, 3);
-            if (bit_read(&opb, 1))
-                cascade |= (bit_read(&opb, 5) << 3);
-            acc += icount(cascade);
-        }
-        for (j = 0; j < acc; j++)
-            bit_read(&opb, 8);
-    }
-    //maps
-    num = bit_read(&opb, 6) + 1;
-    for (; num > 0; num--) {
-        int submaps = 1;
-        bit_read(&opb, 16);    // maps is just mapping0
-        if (bit_read(&opb, 1))
-            submaps = bit_read(&opb, 4) + 1;
-        if (bit_read(&opb, 1))
-            for (i = bit_read(&opb, 8) + 1; i > 0; i--) {
-                bit_read(&opb, rtp_ilog(channels - 1));
-                bit_read(&opb, rtp_ilog(channels - 1));
-            }
-        bit_read(&opb, 2);
-        if (submaps > 1)
-            for (i = 0; i < channels; i++)
-                bit_read(&opb, 4);
-        for (i = 0; i < submaps; i++) {
-            bit_read(&opb, 8);
-            bit_read(&opb, 8);
-            bit_read(&opb, 8);
-        }
-    }
-
-    //modes
-    vorb->modes = bit_read(&opb, 6) + 1;
-    for (i = 0; i < vorb->modes; i++) {
-        vorb->param_blockflag[i] = bit_read(&opb, 1);    //blockflag
-        bit_read(&opb, 16);
-        bit_read(&opb, 16);
-        bit_read(&opb, 8);
-    }
-
-    return 0;        //FIXME add some checks and return -1 on failure
-}
-
-//get the blocksize from a full data packet
-static long pkt_blocksize(rtp_vorbis * vorb, rtp_frame * fr)
-{
-    int mode;
-    bit_context bc;
-    bit_readinit(&bc, fr->data, fr->len);
-    bit_read(&bc, 1);
-
-    mode = bit_read(&bc, rtp_ilog(vorb->modes));
-
-    return vorb->blocksizes[vorb->param_blockflag[mode]];
-}
 
 static int single_parse(rtp_vorbis * vorb, rtp_pkt * pkt, rtp_frame * fr,
             rtp_buff * config, rtp_ssrc * ssrc)
@@ -300,8 +61,6 @@ static int single_parse(rtp_vorbis * vorb, rtp_pkt * pkt, rtp_frame * fr,
         (RTP_XIPH_T(pkt) != 1)    //not a config packet
        )
         return RTP_PARSE_ERROR;
-
-
 
     fr->data = vorb->buf = realloc(vorb->buf, len);
     fr->len = vorb->len = len;
@@ -318,10 +77,6 @@ static int single_parse(rtp_vorbis * vorb, rtp_pkt * pkt, rtp_frame * fr,
     else {
         config->data = vorb->conf[0].conf;
         config->len  = vorb->conf[0].len;
-        vorb->curr_bs = pkt_blocksize(vorb, fr);
-        if (vorb->prev_bs)
-            fr->timestamp += (vorb->curr_bs + vorb->prev_bs) / 4;
-        vorb->prev_bs = vorb->curr_bs;
     }
 
     return 0;
@@ -330,7 +85,7 @@ static int single_parse(rtp_vorbis * vorb, rtp_pkt * pkt, rtp_frame * fr,
 static int frag_parse(rtp_vorbis * vorb, rtp_pkt * pkt, rtp_frame * fr,
               rtp_buff * config, rtp_ssrc * ssrc)
 {
-    int len, err = 0;
+    int len, err = EAGAIN;
 
     switch (RTP_XIPH_F(pkt)) {
     case 1:
@@ -359,11 +114,7 @@ static int frag_parse(rtp_vorbis * vorb, rtp_pkt * pkt, rtp_frame * fr,
         else {
             config->data = vorb->conf[0].conf;
             config->len  = vorb->conf[0].len;
-            vorb->curr_bs = pkt_blocksize(vorb, fr);
-            if (vorb->prev_bs)
-                fr->timestamp +=
-                    (vorb->curr_bs + vorb->prev_bs) / 4;
-            vorb->prev_bs = vorb->curr_bs;
+            err = 0;
         }
         break;
     default:
@@ -402,7 +153,7 @@ static int xiphrtp_to_mkv(rtp_vorbis *vorb, uint8_t *value, int len, int id)
 {
     uint8_t *cur = value, *conf;
     rtp_xiph_conf *tmp;
-    int err = 1, i, count, offset = 1, off = 0, val;
+    int i, count, offset = 1, off = 0, val;
     if (len) {
         // convert the format
         count = get_v(&cur, len);
@@ -423,8 +174,6 @@ static int xiphrtp_to_mkv(rtp_vorbis *vorb, uint8_t *value, int len, int id)
         /*fprintf(stderr,"len %d\n",len);*/
         conf[0] = count;
         memcpy(conf + offset, cur, len);
-        // parse it
-        err = cfg_parse(vorb, cur, len, off);
         // append to the list
         vorb->conf_len++;
         vorb->conf = realloc(vorb->conf,
@@ -434,7 +183,7 @@ static int xiphrtp_to_mkv(rtp_vorbis *vorb, uint8_t *value, int len, int id)
         tmp->len = len + offset;
         tmp->id = id;
     }
-    return err;
+    return 0;
 }
 
 static int unpack_config(rtp_vorbis *vorb, char *value, int len)
