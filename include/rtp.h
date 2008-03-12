c23b6c01fcdac23af4c15c705ab5b01ca59b8375
Luca Barbato <lu_zero@gentoo.org>
2008-03-12 15:06:57 +0100

    avoid implicit enum declaration

--- include/rtp.h
+++ include/rtp.h
@@ -158,6 +158,8 @@ typedef struct {
 #define RTP_TRANSPORT_SET                0
 #define RTP_TRANSPORT_ERR                1
 
+enum deliveries { unicast, multicast };
+
 typedef struct {
     char *spec;
     uint32_t ssrc;
@@ -166,7 +168,7 @@ typedef struct {
     int append;
     int layers;
     int ttl;
-    enum deliveries { unicast, multicast } delivery;
+    enum deliveries delivery;
     nms_transport RTP;
     nms_transport RTCP;
 } rtp_transport;
