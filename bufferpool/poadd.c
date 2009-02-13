e9aa381189918c1189cd2b13f785c586055120da
Marco Pasin <marco.pasin@polito.it>
2009-02-13 16:05:34 +0100

    Fix for SEQ wraparound and list insertions

--- bufferpool/poadd.c
+++ bufferpool/poadd.c
@@ -1,9 +1,9 @@
-/* * 
+/* *
  * This file is part of libnemesi
  *
  * Copyright (C) 2007 by LScube team <team@streaming.polito.it>
  * See AUTHORS for more details
- * 
+ *
  * libnemesi is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -17,11 +17,15 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with libnemesi; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *  
+ *
  * */
 
 #include "rtp.h"
 #include "bufferpool.h"
+
+#define po_get_pkg(index) ((rtp_pkt *) (*(po->bufferpool) + index))
+#define SEQ_MAX_JUMP 0xF000
+#define SEQ_MAX_NUMBER 0x1000
 
 /*!
 * \brief Inserisce un elemento nel Buffer di Playout.
@@ -42,27 +46,31 @@ int poadd(playout_buff * po, int index, uint32_t cycles)
 {
     int i;
     uint32_t cseq;
+    uint32_t r_cycles;
 
     pthread_mutex_lock(&(po->po_mutex));
 
+    r_cycles = po->cycles;
     i = po->pohead;
+    cseq = ntohs(po_get_pkg(index)->seq) + cycles;
 
-    cseq =
-        (uint32_t) ntohs(((rtp_pkt *) (*(po->bufferpool) + index))->seq) +
-        cycles;
-    while ((i != -1)
-           && ((uint32_t) ntohs(((rtp_pkt *) (*(po->bufferpool) + i))->seq) +
-           po->cycles > cseq)) {
+    while ( (i != -1) && ((ntohs(po_get_pkg(i)->seq) + r_cycles) > cseq) ) {
+        rtp_pkt * current_pkg = po_get_pkg(i);
+        rtp_pkt * next_pkg = po_get_pkg(po->pobuff[i].next);
+
+        if( ntohs(next_pkg->seq) - ntohs(current_pkg->seq) >= SEQ_MAX_JUMP) {
+           r_cycles -= SEQ_MAX_NUMBER;
+        }
+
         i = po->pobuff[i].next;
     }
-    if ((i != -1)
-        && (cseq ==
-        ((uint32_t) ntohs(((rtp_pkt *) (*(po->bufferpool) + i))->seq) +
-         po->cycles))) {
+
+    if ( (i != -1) && ((ntohs(po_get_pkg(i)->seq) + r_cycles) == cseq) ) {
         pthread_mutex_unlock(&(po->po_mutex));
         return PKT_DUPLICATED;
     }
-    if (i == po->pohead) {    /* inserimento in testa */
+
+    if (i == po->pohead) {    /* head insert */
         po->pobuff[index].next = i;
         po->pohead = index;
         if (i == -1)
@@ -74,18 +82,19 @@ int poadd(playout_buff * po, int index, uint32_t cycles)
 
         po->pocount++;
     } else {
-        if (i == -1) {    /* inserimento in coda */
+        if (i == -1) {    /* tail insert */
             i = po->potail;
             po->potail = index;
-        } else        /* inserimento */
-            po->pobuff[po->pobuff[i].next].prev = index;
-
-        po->pobuff[index].next = po->pobuff[i].next;
-        po->pobuff[i].next = index;
-        po->pobuff[index].prev = i;
-
+            po->pobuff[index].next = po->pobuff[i].next;
+            po->pobuff[i].next = index;
+            po->pobuff[index].prev = i;
+        } else {       /* Inserting before the current one (see the while loop) */
+            po->pobuff[index].prev = po->pobuff[i].prev;
+            po->pobuff[index].next = i;
+            po->pobuff[po->pobuff[i].prev].next = index;
+            po->pobuff[i].prev = index;
+        }
         po->pocount++;
-
         pthread_mutex_unlock(&(po->po_mutex));
         return PKT_MISORDERED;
     }
